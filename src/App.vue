d889b369630f7f551531fede67ae578855cac1fc
Matt <60991158+matthewfee@users.noreply.github.com>
2022-08-24 12:21:40 +0200

    added individual components and quesiton index

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,133 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
-  <div id="app" class="contain"></div>
+  <div id="app" class="contain">
+    <GamestateStart v-if="uiState === 'start'">
+      <h2>Which hooman do you want to be?</h2>
+
+      <p v-for="option in characterChoices" :key="option" for="option" class="character-choices">
+        <input v-model="characterInput" type="radio" :id="option" :value="option" />
+        <label :for="option">{{ option }}</label>
+      </p>
+      <button @click="pickCharacter">Pick Your Character</button>
+    </GamestateStart>
+
+    <section v-else>
+      <svg viewBox="0 -180 1628 1180" class="main">
+        <defs>
+          <clipPath id="bottom-clip">
+            <rect class="bottom-clip-path" x="1131.5" y="546.5" width="406" height="1000" />
+          </clipPath>
+          <clipPath id="top-clip">
+            <rect class="top-clip-path" x="1131.5" y="69.5" width="406" height="473" />
+          </clipPath>
+        </defs>
+
+        <Friend></Friend>
+        <Score></Score>
+
+        <component :is="character"></component>
+        <text
+          x="1000"
+          y="930"
+          style="font: normal 45px 'Recursive; text-transform: uppercase;"
+          class="text"
+        >
+          {{ character }}
+        </text>
+
+        <path fill="#f0959f" d="M0 842h657v192H0z" />
+
+        <g id="friendbubble">
+          <path
+            class="cls-45"
+            d="M342.3 471.8h106.6c0 42.6-21.3 63.9-21.3 63.9 64 0 85.3-63.9 85.3-63.9h42.6c42.7 0 42.7-42.7 42.7-42.7s21.3-149.3 21.3-191.9-42.6-42.7-42.6-42.7H321c-42.7 0-42.7 42.7-42.7 42.7l21.4 191.9s0 42.7 42.6 42.7z"
+            transform="translate(17)"
+          />
+          <path
+            class="cls-20"
+            d="M333.8 463.2h106.6c0 42.7-21.3 64-21.3 64 63.9 0 85.3-64 85.3-64H547c42.7 0 42.7-42.6 42.7-42.6s21.3-149.3 21.3-192-42.7-42.6-42.7-42.6H312.4c-42.6 0-42.6 42.6-42.6 42.6l21.3 192s0 42.6 42.7 42.6z"
+            transform="translate(17)"
+          />
+        </g>
+
+        <g id="alienbubble">
+          <path
+            class="cls-45"
+            d="M948.3 344.5c-103.8 0-187.9 76.3-187.9 170.4s84.1 170.3 187.9 170.3a201.5 201.5 0 00100.5-26.4l87.4 26.4-29.1-79.2c18.4-26.4 29.1-57.6 29.1-91.1 0-94.1-84.1-170.4-187.9-170.4z"
+            transform="translate(17)"
+          />
+          <path
+            class="cls-20"
+            d="M938.9 336C835.1 336 751 412.3 751 506.4s84.1 170.3 187.9 170.3a201.5 201.5 0 00100.5-26.4l87.4 26.4-29.1-79.2c18.4-26.4 29.1-57.6 29.1-91.1 0-94.1-84.1-170.4-187.9-170.4z"
+            transform="translate(17)"
+          />
+        </g>
+      </svg>
+
+      <div class="friendtalk">
+        <h3>{{ questions[questionIndex].question }}</h3>
+      </div>
+
+      <div class="zombietalk">
+        <p v-for="character in characterChoices" :key="character">
+          <button @click="pickQuestion(character)">
+            {{ questions[questionIndex][character] }}
+          </button>
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script></script>
+<script>
+import Artist from "@/components/Artist.vue";
+import Baker from "@/components/Baker.vue";
+import Friend from "@/components/Friend.vue";
+import Mechanic from "@/components/Mechanic.vue";
+import Score from "@/components/Score.vue";
+import Zombie from "@/components/Zombie.vue";
+
+import { mapState } from "vuex";
+
+import GamestateStart from "./components/GamestateStart.vue";
+
+export default {
+  components: {
+    GamestateStart,
+    Artist,
+    Baker,
+    Friend,
+    Mechanic,
+    Score,
+    Zombie,
+  },
+  data() {
+    return {
+      characterInput: "",
+    };
+  },
+
+  computed: {
+    ...mapState([
+      "uiState",
+      "questions",
+      "questionIndex",
+      "score",
+      "character",
+      "characterChoices",
+    ]),
+  },
+  methods: {
+    pickCharacter() {
+      this.$store.commit("pickCharacter", this.characterInput);
+      this.$store.commit("updateUIState", "characterChosen");
+    },
+    pickQuestion(character) {
+      this.$store.commit("pickQuestion", character);
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 body {
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,70 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    uiState: "start",
+    characterChoices: ["baker", "mechanic", "artist"],
+    character: "",
+    questionIndex: 0,
+    score: 0,
+    questions: [
+      {
+        question: "What's your dog's name?",
+        baker: "Woofgang Puck",
+        mechanic: "Alf",
+        artist: "Salvador Dogi",
+      },
+      {
+        question: "What's your favorite hobby?",
+        baker: "Extreme ironing",
+        mechanic: "Bacon santa cosplay",
+        artist: "Mimosas",
+      },
+      {
+        question: "What's your favorite color?",
+        baker: "turquoise",
+        mechanic: "yellow",
+        artist: "transparent",
+      },
+      {
+        question: "Is cereal soup?",
+        baker: "You can't be serieal",
+        mechanic: "Yes, I am Jason Lengstorf",
+        artist: "wut",
+      },
+      {
+        question: "What’s the most imaginative insult you can come up with?",
+        baker: "You're a substitute teacher with no lesson plan",
+        mechanic: "Yer face is a melted welly",
+        artist: "You eat buttons off the remote",
+      },
+      {
+        question: "If peanut butter wasn’t called peanut butter, what would it be called?",
+        baker: "legoome",
+        mechanic: "brown paste",
+        artist: "groundnut smoosh",
+      },
+    ],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  mutations: {
+    pickCharacter(state, character) {
+      state.character = character;
+    },
+    updateUIState(state, newUIState) {
+      state.uiState = newUIState;
+    },
+    pickQuestion(state, character) {
+      // eslint-disable-next-line no-unused-expressions
+
+      console.log(character);
+      // eslint-disable-next-line no-unused-expressions
+      character === state.character ? (state.score += 10) : (state.score -= 10);
+
+      if (state.questionIndex < state.questions.length - 1) {
+        // eslint-disable-next-line no-plusplus
+        state.questionIndex++;
+      }
+    },
+  },
 });
